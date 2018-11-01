92a4e49614b0386a4d22e385c0489218920c9f33
Giovanni <giovanni.montenegroit@gmail.com>
2018-11-01 12:47:59 +0100

    Added React hooks example

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,16 +1,43 @@
 import * as React from 'react'
 import {ThemeContext} from '../context/theme'
 
+type TestProps = {
+    onThemeChange: Function 
+}
+
+function Test (props: TestProps){
+    const context = React.useContext(ThemeContext);
+   return (
+   <div>
+    Actual theme: {context.theme}
+    <button onClick={() => props.onThemeChange()}>Change Theme</button>
+</div>)
+}
 
 export default function App(){
-    const [theme, setTheme] = React.useState('dark');
-    return (
-    <div>
+    const [theme, setTheme] = React.useState({theme:'dark'});
+    const [themee, setThemee] = React.useState({themee:'dark'});
+    
+    React.useEffect( () => {
+        console.log("Theme changed dude!")
+    },[theme])
+
+    React.useEffect( () => {
+        console.log("Themee changed dude!")
+    },[themee])
+    React.useEffect( () => {
         
-        <ThemeContext.Consumer>
-            {(value)=> value.theme}
-        </ThemeContext.Consumer>
-    </div>
+        console.log("Themee e Theme changed dude!",theme,themee)
+    },[themee,theme])
+
+    function onThemeChange(){
+        setThemee({themee:'light'})
+    }
+
+    return (
+        <ThemeContext.Provider value={theme}>
+            <Test onThemeChange={onThemeChange}/>
+        </ThemeContext.Provider>
     )
 }
 
